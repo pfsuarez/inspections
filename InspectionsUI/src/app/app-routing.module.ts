98bfdc8c7fa7bd588abbf5184fc37bdae0a63f6b
pfsuarez <pfsuarez@gmail.com>
2020-01-28 08:39:39 -0300

    Added inspections list and edit components. Added routing

--- InspectionsUI/src/app/app-routing.module.ts
+++ InspectionsUI/src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EditComponent } from './inspection/edit/edit.component';
+import { MainComponent } from './inspection/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inspections',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inspections',
+    component: MainComponent
+  },
+  {
+    path: 'create',
+    component: EditComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'inspections'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- InspectionsUI/src/app/app.module.ts
+++ InspectionsUI/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './inspection/main/main.component';
+import { EditComponent } from './inspection/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
